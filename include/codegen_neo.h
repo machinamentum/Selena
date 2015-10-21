08962c354fcbcfb1031f5f6fa4966f898e9de97c
Josh Huelsman <joshuahuelsman@gmail.com>
2015-10-21 19:59:38 -0400

    Add mov instruction generation to nihcode generator

--- include/codegen_neo.h
+++ include/codegen_neo.h
@@ -7,29 +7,110 @@
 #include <vector>
 #include <ostream>
 
-struct neocode_instruction {
+struct neocode_register_file {
+  int Vertex[8];
+  int Constants[96];
+  int Temp[16];
+  int Output[8];
 
-  enum { INLINE };
+  int AllocOutput() {
+    for (int i = 0; i < 8; ++i) {
+      if (!Output[i]) {
+        Output[i] = 1;
+        return i;
+      }
+    }
 
-  int Type;
-  int DstRegister;
-  int Src1Register;
-  int Src2Register;
-  int SwizzleType;
-  std::string ExtraData;
+    return -1;
+  }
+
+  int AllocVertex() {
+    for (int i = 0; i < 8; ++i) {
+      if (!Vertex[i]) {
+        Vertex[i] = 1;
+        return i;
+      }
+    }
+
+    return -1;
+  }
+
+  int AllocTemp() {
+    for (int i = 0; i < 16; ++i) {
+      if (!Temp[i]) {
+        Temp[i] = 1;
+        return i + 0x10;
+      }
+    }
+
+    return -1;
+  }
+
+  int AllocConstant() {
+    for (int i = 0; i < 96; ++i) {
+      if (!Constants[i]) {
+        Constants[i] = 1;
+        return i + 0x20;
+      }
+    }
+
+    return -1;
+  }
+
+  void Free(int Register) {
+    if (Register < 0x8)
+      Vertex[Register] = 0;
+
+    if (Register >= 0x10 && Register < 0x20)
+      Temp[Register - 0x10] = 0;
+
+    if (Register >= 0x20)
+      Constants[Register - 0x20] = 0;
+  }
 };
 
 struct neocode_variable {
-  int Type;
+
+  enum {
+    OUTPUT_POSITION = 1,
+    OUTPUT_QUATERNION,
+    OUTPUT_COLOR,
+    OUTPUT_TEXCOORD0,
+    OUTPUT_TEXCOORD1,
+    OUTPUT_TEXCOORD2,
+    OUTPUT_VIEW
+  };
+
+  std::string Name;
   std::string TypeName;
+  int Type;
   int Register;
+  int RegisterType;
 };
 
+struct neocode_instruction {
+
+  enum { INLINE, MOV };
+
+  int Type;
+  neocode_variable Dst;
+  neocode_variable Src1;
+  neocode_variable Src2;
+  int Swizzle;
+  std::string ExtraData;
+};
+
+struct neocode_program;
+
 struct neocode_function {
+  neocode_program *Program;
   int ReturnType;
   std::string Name;
   std::vector<neocode_variable> Variables;
   std::vector<neocode_instruction> Instructions;
+
+  neocode_function(neocode_program *P) : Program(P) {}
+  neocode_variable *GetVariable(std::string Name);
 };
 
 struct neocode_constant {
@@ -57,6 +138,8 @@ struct neocode_constant {
 struct neocode_program {
   std::vector<neocode_constant> Constants;
   std::vector<neocode_function> Functions;
+  std::vector<neocode_variable> Globals;
+  neocode_register_file Registers;
 };
 
 neocode_program CGNeoBuildProgramInstance(ast_node *ASTNode);
--- source/codegen_neo.cpp
+++ source/codegen_neo.cpp
@@ -1,6 +1,51 @@
 
 #include "codegen_neo.h"
 
+#ifdef _3DS
+#include <sstream>
+namespace std {
+template <typename T> string to_string(T Value) {
+  stringstream ss;
+  ss << Value;
+  return ss.str();
+}
+};
+#endif
+
+static std::string RegisterName(neocode_variable &Var) {
+  if (Var.RegisterType == 0) {
+    int Register = Var.Register;
+    if (Register < 0x10)
+      return std::string("v") + std::to_string(Register);
+    if (Register < 0x20)
+      return std::string("r") + std::to_string(Register - 0x10);
+
+    return std::string("c") + std::to_string(Register - 0x20);
+  } else {
+    int Register = Var.Register;
+    if (Register < 0x10)
+      return std::string("o") + std::to_string(Register);
+    if (Register < 0x20)
+      return std::string("r") + std::to_string(Register - 0x10);
+
+    return "URV";
+  }
+}
+
+neocode_variable *neocode_function::GetVariable(std::string Name) {
+  for (neocode_variable &V : Variables) {
+    if (V.Name.compare(Name) == 0) {
+      return &V;
+    }
+  }
+
+  for (neocode_variable &V : Program->Globals) {
+    if (V.Name.compare(Name) == 0) {
+      return &V;
+    }
+  }
+}
+
 void CGNeoBuildStatement(neocode_function *Function, ast_node *ASTNode) {
   if (ASTNode->Type == ast_node::FUNCTION_CALL) {
     if (ASTNode->Id.compare("asm") == 0) {
@@ -10,10 +55,19 @@ void CGNeoBuildStatement(neocode_function *Function, ast_node *ASTNode) {
       Function->Instructions.push_back(In);
     }
   }
+
+  if (ASTNode->Type == ast_node::ASSIGNMENT) {
+    neocode_instruction In;
+    In.Type = neocode_instruction::MOV;
+    In.Dst = *Function->GetVariable(ASTNode->Id);
+    In.Src1 = *Function->GetVariable(ASTNode->Children[0].Id);
+    Function->Instructions.push_back(In);
+  }
 }
 
-neocode_function CGNeoBuildFunction(ast_node *ASTNode) {
-  neocode_function Function;
+neocode_function CGNeoBuildFunction(neocode_program *Program,
+                                    ast_node *ASTNode) {
+  neocode_function Function = neocode_function(Program);
   Function.Name = ASTNode->Id;
   for (int i = 1; i < ASTNode->Children.size(); ++i) {
     CGNeoBuildStatement(&Function, &ASTNode->Children[i].Children[0]);
@@ -23,9 +77,21 @@ neocode_function CGNeoBuildFunction(ast_node *ASTNode) {
 
 neocode_program CGNeoBuildProgramInstance(ast_node *ASTNode) {
   neocode_program Program;
+  Program.Globals.push_back((neocode_variable){
+      "gl_Position", "vec4", ast_node::STRUCT, Program.Registers.AllocOutput(),
+      neocode_variable::OUTPUT_POSITION});
+  Program.Globals.push_back((neocode_variable){
+      "gl_FrontColor", "vec4", ast_node::STRUCT,
+      Program.Registers.AllocOutput(), neocode_variable::OUTPUT_COLOR});
+  Program.Globals.push_back(
+      (neocode_variable){"gl_Vertex", "vec4", ast_node::STRUCT,
+                         Program.Registers.AllocVertex(), 0});
+  Program.Globals.push_back(
+      (neocode_variable){"gl_Color", "vec4", ast_node::STRUCT,
+                         Program.Registers.AllocVertex(), 0});
   for (ast_node &Node : ASTNode->Children) {
     if (Node.Type == ast_node::FUNCTION)
-      Program.Functions.push_back(CGNeoBuildFunction(&Node));
+      Program.Functions.push_back(CGNeoBuildFunction(&Program, &Node));
   }
   return Program;
 }
@@ -35,6 +101,12 @@ void CGNeoGenerateInstruction(neocode_instruction *Instruction,
   switch (Instruction->Type) {
   case neocode_instruction::INLINE:
     os << " " << Instruction->ExtraData << std::endl;
+    break;
+
+  case neocode_instruction::MOV:
+    os << " "
+       << "mov " << RegisterName(Instruction->Dst) << ", "
+       << RegisterName(Instruction->Src1) << std::endl;
     break;
 
   default:
@@ -51,13 +123,25 @@ void CGNeoGenerateFunction(neocode_function *Function, std::ostream &os) {
   os << Function->Name << "_end:" << std::endl;
 }
 
+static std::string OutputName(int Register) {
+  const std::string ONames[7] = {
+      "position",  "quaternion", "color", "texcoord0",
+      "texcoord1", "texcoord2",  "view",
+  };
+
+  return ONames[Register - 1];
+}
+
 void CGNeoGenerateCode(neocode_program *Program, std::ostream &os) {
   os << ".alias CaelinaCCVersion c95 as (0.0, 0.0, 0.0, 0.1)" << std::endl;
-  os << ".alias outpos o0 as position" << std::endl;
-  os << ".alias outcol o1 as color" << std::endl;
-  os << ".alias outtex0 o2.xyzw as texcoord0" << std::endl;
-  os << ".alias outtex1 o3.xyzw as texcoord1" << std::endl;
-  os << ".alias outtex2 o4.xyzw as texcoord2" << std::endl;
+  for (neocode_variable &V : Program->Globals) {
+    if (V.RegisterType > 0) {
+      os << ".alias " << V.Name << " " << RegisterName(V) << " as "
+         << OutputName(V.RegisterType) << std::endl;
+    } else {
+      os << ".alias " << V.Name << " " << RegisterName(V) << std::endl;
+    }
+  }
   for (neocode_function &Function : Program->Functions) {
     CGNeoGenerateFunction(&Function, os);
   }
